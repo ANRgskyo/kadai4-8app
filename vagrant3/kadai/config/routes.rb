4c66614e32f3af40cc9c9336915f0aa4415080e9
ANRgskyo <RAngskyo@gmail.com>
2018-12-09 15:24:19 +0900

    [Finish]4-8

---
+++ vagrant3/kadai/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'books#top'
+  get '/books' => 'books#index'
+
+  get '/books/:id' => 'books#show', as: 'book'
+  post '/books' => 'books#create'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch '/books/:id' => 'books#update', as: 'update_book'
+  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
+  
+end
